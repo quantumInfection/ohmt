bbe66827cd3a55d7ddd498f1097ebcdf77fe7f51
Abdullah Aftab <a13dullah.a@gmail.com>
2024-08-28 02:43:13 +0500

    Run black

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def public():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
